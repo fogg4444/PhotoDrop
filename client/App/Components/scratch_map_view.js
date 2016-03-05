e17c32cb2ea14bacc20917678e8afe5d9a34a111
Brian Fogg <fogg4444@gmail.com>
2016-03-05 11:41:29 -0800

    Remove scratch files

--- client/App/Components/scratch_map_view.js
+++
@@ -1,8 +0,0 @@
-          { this.state.closeAudioLocations.map((audioLocation) => {
-              return (
-               <MapView.Marker coordinate={{latitude: audioLocation.loc.coordinates[1], longitude: audioLocation.loc.coordinates[0]}} onPress={this.showAudio(audioLocation._id, audioLocation.text)}>
-                 <RedAudioMarker navigator={this.props.navigator}/>
-               </MapView.Marker>
-             )}
-            )
-          }
